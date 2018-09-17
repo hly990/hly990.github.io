785aecbbd1bf42bad1cb29d2c2bca4c9b18df9d6
He <hsl990@163.com>
2018-09-17 18:59:43 +0800

    cc

--- video-player/video.js
+++ video-player/video.js
@@ -8478,7 +8478,7 @@
       right: 0,
       top: 0,
       bottom: 0,
-      display: "inline",
+      display: "block",
       writingMode: cue.vertical === "" ? "horizontal-tb" : cue.vertical === "lr" ? "vertical-lr" : "vertical-rl",
       unicodeBidi: "plaintext"
     };
